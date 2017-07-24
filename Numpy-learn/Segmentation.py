36feb3c9fe4250edf91de38d2b190b12f72cb718
philex5 <philexwoo@gmail.com>
2017-07-24 15:21:10 +0800

    litter change

--- Numpy-learn/Segmentation.py
+++ Numpy-learn/Segmentation.py
@@ -14,5 +14,5 @@ print(np.split(A,2,axis=1))
 print(np.split(A,3,axis=0))
 
 print(np.array_split(A,3,axis=1))
-print(np.hsplit(A,2))
-print(np.vsplit(A,3))
+print(np.hsplit(A, 2))
+print(np.vsplit(A, 3))
--- Pandas-learn/concatTest.py
+++ Pandas-learn/concatTest.py
@@ -10,26 +10,26 @@ import pandas as pd
      -append： 添加数据
 
 """
-df1=pd.DataFrame(np.ones((3,4))*0,columns=['a','b','c','d'])
-df2=pd.DataFrame(np.ones((3,4))*1,columns=['a','b','c','d'])
-df3=pd.DataFrame(np.ones((3,4))*2,columns=['d','f','g','h'])
+df1=pd.DataFrame(np.ones((3, 4))*0, columns=['a', 'b', 'c', 'd'])
+df2=pd.DataFrame(np.ones((3, 4))*1, columns=['a', 'b', 'c', 'd'])
+df3=pd.DataFrame(np.ones((3, 4))*2, columns=['d', 'f', 'g', 'h'])
 
 """
 cancat纵向合并(axis=0)
 ignore_index=True,重置index
 
 """
-res=pd.concat([df1,df2,df3],axis=0)
+res=pd.concat([df1, df2, df3], axis=0)
 print(res)
 
-res=pd.concat([df1,df2,df3],axis=0,ignore_index=True)
+res=pd.concat([df1, df2, df3], axis=0, ignore_index=True)
 print(res)
 
 
-print(pd.concat([df1,df3],axis=0))
-print(pd.concat([df1,df3],axis=1,join_axes=[df1.index]))
+print(pd.concat([df1, df3], axis=0))
+print(pd.concat([df1, df3], axis=1, join_axes=[df1.index]))
 
 """只留下共有的"""
-print(pd.concat([df1,df3],axis=0,join="inner"))
+print(pd.concat([df1, df3], axis=0, join="inner"))
 
-print(df1.append(df2,ignore_index=True))
+print(df1.append(df2, ignore_index=True))
