b5b35d3df5249e35504ba7cb4e3af75e4c9ee337
philex5 <philexwoo@gmail.com>
2017-07-26 20:23:55 +0800

    test

---
+++ Python-learn/7.Debug&Test/4.文档测试.py
@@ -0,0 +1,41 @@
+"""
+     Python内置的“文档测试”（doctest）模块可以直接提取注释中的代码并执行测试
+     doctest非常有用，不但可以用来测试，还可以直接作为示例代码。通过某些文档生成工具，就可以自动把包含doctest的注释提取出来。用户看文档的时候，同时也看到了doctest。
+
+"""
+class Dict(dict):
+
+    '''
+    simple dict but also support access as x.y style
+
+    >>> d1 =Dict()
+    >>> d1['x'] = 100
+    >>> d1.x
+    100
+    >>> d1.y = 200
+    >>> d1['y']
+    200
+    >>> d2 = Dict(a=1, b=2, c='3')
+    >>> d2.c
+    '3'
+    >>> d2['empty']
+    Traceback (most recent call last):
+       ...
+    KeyError: 'empty'
+    '''
+
+    def __init__(self, **kw):
+        super(Dict, self).__init__(**kw)
+
+    # def __getattr__(self, key):
+    #     try:
+    #         return self[key]
+    #     except KeyError:
+    #         raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+if __name__ == 'main':
+    import doctest
+    doctest.testmod()
